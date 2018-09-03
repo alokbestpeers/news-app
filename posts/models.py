fdb642a755f55309ed73ed11b2ba9298189c6ac1
alokbestpeers <aramteke@bestpeers.com>
2018-09-03 19:51:29 +0530

    created models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import Truncator
 
-# Create your models here.
+
+class News(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField(max_length=10000)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+	def shortContent(self):
+		shortContent = Truncator(self.content)
+		return shortContent.chars(100)
+
+
+
