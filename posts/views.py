4f7212189b68e95a79278ad10cba940c0584ebdf
alokbestpeers <aramteke@bestpeers.com>
2018-09-04 15:18:39 +0530

    created view for full news page

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import News
 
 def home(request):
 	news = News.objects.all()
 	return render(request, 'home.html', {'news':news})
+
+@login_required
+def news(request, pk):
+	news = get_object_or_404(News, pk=pk)
+	return render(request, 'news.html', {'news': news})
